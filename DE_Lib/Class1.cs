7552254f8af00da46a144ff08081cf4ce22d2ff0
DmtrDv <dmitriybychkov75146@gmail.com>
2026-03-03 15:02:25 +0300

    Добавьте файлы проекта.

---
+++ DE_Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DE_Lib
+{
+    public class Class1
+    {
+    }
+}
